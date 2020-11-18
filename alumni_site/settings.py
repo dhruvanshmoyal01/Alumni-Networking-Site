844dc78b7be620f45e01057ac1b4b20bb7602d69
dhruvanshmoyal01 <dhruvanshm.official@gmail.com>
2020-11-18 20:48:51 +0530

    reseting password feat. added

--- alumni_site/settings.py
+++ alumni_site/settings.py
@@ -133,3 +133,12 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'index-page'
 
 LOGIN_URL = 'login'
+
+#SMTP configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'dhruvansh.abhivyakti@gmail.com'
+EMAIL_HOST_PASSWORD = '3cho1234'
--- alumni_site/urls.py
+++ alumni_site/urls.py
@@ -45,6 +45,11 @@ urlpatterns = [
     path('profile_all/unfollow/<int:pk>/', user_views.unfollow, name = 'unfollow-user'),
     path('my_activity/', user_views.myactivity, name = 'my-acivity'),
 
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name='user/password_reset.html'), name = 'password_reset'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name='user/password_reset_sent.html'), name = 'password_reset_done'),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name='user/password_reset_form.html'), name = 'password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='user/password_reset_done.html'), name = 'password_reset_complete')
+
 ]
 
 if settings.DEBUG:
